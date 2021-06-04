b283d77c36a0212cf5155f0fa0102dde51bd2aee
Konrad <konradhbr@gmail.com>
2021-06-04 18:25:43 +0200

    fix bugs

--- src/components/HomePage/HomePage.vue
+++ src/components/HomePage/HomePage.vue
@@ -21,7 +21,7 @@
       <div class="home__icon">
         <div class="icon">
           <a href="#news" id="goNews">
-            <img src="/src/assets/icons/scroll-icon.svg" alt="scroll" />
+            <img src="../../assets/icons/scroll-icon.svg" alt="scroll" />
           </a>
         </div>
       </div>
